fc156a3ebc31d100e4cc979710cdfb30e87accf5
Zlobin Vladimir <vladimir.zlobin@intel.com>
2019-11-07 17:09:08 +0300

    use timeout instead of sleep and kill, wmctrl->xdotool

--- keyfreq.sh
+++ keyfreq.sh
@@ -11,22 +11,12 @@ helperfile='/dev/shm/keyfreqraw.txt'
 
 mkdir -p logs
 
-trap 'kill $(jobs -p)' EXIT
-
 while true
 do
-  xinput test "${ULOGME_KEYBOARD_ID:-11}" | tr -d '0-9' > "$helperfile" &
+  timeout 9 xinput test "${ULOGME_KEYBOARD_ID:-11}" | tr -d '0-9' > "$helperfile"
 
   ## In case you can not get `xinput` to work. Note that you will need to run `showkey` as root.
-  # showkey | tr -d '0-9' > $helperfile &
-
-  # Work in windows of 9 seconds
-  sleep 9
-
-  # shellcheck disable=SC2046
-  kill $(jobs -rp)
-  # shellcheck disable=SC2046
-  wait $(jobs -rp) 2>/dev/null
+  # timeout 9 showkey | tr -d '0-9' > $helperfile
 
   # Count number of key release events
   num="$(grep --count release "$helperfile")"
@@ -35,5 +25,4 @@ do
   logfile="logs/keyfreq_$(python rewind7am.py).txt"
   echo "$(date +%s) $num" >> "$logfile"
   echo "logged key frequency: $(date) $num release events detected into $logfile"
-
 done
